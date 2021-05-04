ba80bbff3a9218b01168c865ea6b022a4416388d
Kevin Tran <kevin_tran@live.fr>
2021-05-04 23:54:30 +0200

    Use prettier for eslint

--- settings/typescript/___eslintrc.js
+++ settings/typescript/___eslintrc.js
@@ -1,39 +1,38 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: [
-        "plugin:vue/essential",
-        "eslint:recommended",
-        "@vue/typescript/recommended",
-    ],
-    parserOptions: {
-        ecmaVersion: 2020,
-    },
-    rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "@typescript-eslint/no-useless-constructor": "error",
-        "no-useless-constructor": "off",
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        "comma-dangle": ["error", "always-multiline"],
-        "space-before-function-paren": ["error", "never"],
-        indent: "off",
-        "@typescript-eslint/indent": ["error"],
-        "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
-    },
-    overrides: [
-        {
-            files: [
-                "**/__tests__/*.{j,t}s?(x)",
-                "**/tests/unit/**/*.spec.{j,t}s?(x)",
-            ],
-            env: {
-                jest: true,
-            },
-        },
-    ],
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/no-useless-constructor": "error",
+    "no-useless-constructor": "off",
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "space-before-function-paren": ["error", "never"],
+    indent: "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
